325c03d00a186c2ac64d7c21a55c342e5292cfcf
mh-northlander <mh.northlander@gmail.com>
2021-08-11 10:23:08 +0900

    Add grammar unittest

--- src/dic/grammar.rs
+++ src/dic/grammar.rs
@@ -109,3 +109,130 @@ named_args!(
         (connect_cost)
     )
 );
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn storage_size() {
+        let bytes = setup_bytes();
+        let grammar = Grammar::new(&bytes, 0).expect("failed to create grammar");
+        assert_eq!(bytes.len(), grammar.storage_size);
+    }
+
+    #[test]
+    fn partofspeech_string() {
+        let bytes = setup_bytes();
+        let grammar = Grammar::new(&bytes, 0).expect("failed to create grammar");
+        assert_eq!(6, grammar.pos_list[0].len());
+        assert_eq!("BOS/EOS", grammar.pos_list[0][0]);
+        assert_eq!("*", grammar.pos_list[0][5]);
+
+        assert_eq!("一般", grammar.pos_list[1][1]);
+        assert_eq!("*", grammar.pos_list[1][5]);
+
+        assert_eq!("五段-サ行", grammar.pos_list[2][4]);
+        assert_eq!("終止形-一般", grammar.pos_list[2][5]);
+    }
+
+    #[test]
+    fn get_connect_cost() {
+        let bytes = setup_bytes();
+        let grammar = Grammar::new(&bytes, 0).expect("failed to create grammar");
+        assert_eq!(
+            0,
+            grammar.get_connect_cost(0, 0).expect("failed to get cost")
+        );
+        assert_eq!(
+            -100,
+            grammar.get_connect_cost(2, 1).expect("failed to get cost")
+        );
+        assert_eq!(
+            200,
+            grammar.get_connect_cost(1, 2).expect("failed to get cost")
+        );
+    }
+
+    #[test]
+    fn set_connect_cost() {
+        let bytes = setup_bytes();
+        let mut grammar = Grammar::new(&bytes, 0).expect("failed to create grammar");
+        grammar.set_connect_cost(0, 0, 300);
+        assert_eq!(
+            300,
+            grammar.get_connect_cost(0, 0).expect("failed to get cost")
+        );
+    }
+
+    #[test]
+    fn bos_parameter() {
+        assert_eq!(0, Grammar::BOS_PARAMETER.0);
+        assert_eq!(0, Grammar::BOS_PARAMETER.1);
+        assert_eq!(0, Grammar::BOS_PARAMETER.2);
+    }
+
+    #[test]
+    fn eos_parameter() {
+        assert_eq!(0, Grammar::EOS_PARAMETER.0);
+        assert_eq!(0, Grammar::EOS_PARAMETER.1);
+        assert_eq!(0, Grammar::EOS_PARAMETER.2);
+    }
+
+    #[test]
+    fn read_from_file() {
+        // todo: after tidying up dictionary management
+    }
+
+    fn setup_bytes() -> Vec<u8> {
+        let mut storage: Vec<u8> = Vec::new();
+        build_partofspeech(&mut storage);
+        build_connect_table(&mut storage);
+        storage
+    }
+    fn string_to_bytes(s: &str) -> Vec<u8> {
+        s.encode_utf16()
+            .map(|c| c.to_le_bytes())
+            .flatten()
+            .collect()
+    }
+    fn build_partofspeech(storage: &mut Vec<u8>) -> () {
+        // number of part of speech
+        storage.extend(&(3 as i16).to_le_bytes());
+
+        storage.extend(
+            b"\x07B\x00O\x00S\x00/\x00E\x00O\x00S\x00\x01*\x00\x01*\x00\x01*\x00\x01*\x00\x01*\x00",
+        );
+
+        storage.extend(b"\x02");
+        storage.extend(string_to_bytes("名刺"));
+        storage.extend(b"\x02");
+        storage.extend(string_to_bytes("一般"));
+        storage.extend(b"\x01*\x00\x01*\x00\x01*\x00\x01*\x00");
+
+        storage.extend(b"\x02");
+        storage.extend(string_to_bytes("動詞"));
+        storage.extend(b"\x02");
+        storage.extend(string_to_bytes("一般"));
+        storage.extend(b"\x01*\x00\x01*\x00\x05");
+        storage.extend(string_to_bytes("五段-サ行"));
+        storage.extend(b"\x06");
+        storage.extend(string_to_bytes("終止形-一般"));
+    }
+    fn build_connect_table(storage: &mut Vec<u8>) -> () {
+        storage.extend(&(3 as i16).to_le_bytes());
+        storage.extend(&(3 as i16).to_le_bytes());
+
+        storage.extend(&(0 as i16).to_le_bytes());
+        storage.extend(&(-300 as i16).to_le_bytes());
+        storage.extend(&(300 as i16).to_le_bytes());
+
+        storage.extend(&(300 as i16).to_le_bytes());
+        storage.extend(&(-500 as i16).to_le_bytes());
+        storage.extend(&(-100 as i16).to_le_bytes());
+
+        storage.extend(&(-3000 as i16).to_le_bytes());
+        storage.extend(&(200 as i16).to_le_bytes());
+        storage.extend(&(2000 as i16).to_le_bytes());
+    }
+}
