ed7921901ed40fd77187dccb1f20142eb3e9632f
mh-northlander <mh.northlander@gmail.com>
2021-08-19 19:11:52 +0900

    Add character_category unit test

--- src/dic/character_category.rs
+++ src/dic/character_category.rs
@@ -199,6 +199,10 @@ impl CharacterCategory {
     }
 
     pub fn get_category_types(&self, c: char) -> CategoryTypes {
+        if self.ranges.is_empty() {
+            return FromIterator::from_iter([CategoryType::DEFAULT]);
+        }
+
         let code_point = c as u32;
 
         // binary search
@@ -229,6 +233,10 @@ impl CharacterCategory {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::char;
+    use std::fs::{self, File};
+    use std::io::{BufWriter, Write};
+
     const TEST_RESOURCE_DIR: &str = "./tests/resources/";
     const TEST_CHAR_DEF_FILE: &str = "char.def";
 
@@ -257,7 +265,6 @@ mod tests {
 
     #[test]
     fn get_category_types() {
-        // todo: pass correct path
         let path = PathBuf::from(TEST_RESOURCE_DIR).join(TEST_CHAR_DEF_FILE);
         let cat = CharacterCategory::from_file(path).expect("failed to load char.def for test");
         let cats = cat.get_category_types('熙');
@@ -267,21 +274,228 @@ mod tests {
 
     #[test]
     fn read_character_definition() {
-        // todo: immpl after file read
+        let cats_default = [CategoryType::DEFAULT];
+        let cats_num = [CategoryType::NUMERIC];
+        let cats_alp = [CategoryType::ALPHA];
+        let cats_kanji = [CategoryType::KANJI];
+        let cats_kanji_num = [CategoryType::KANJI, CategoryType::NUMERIC];
+        let cats_kanjinumeric = [CategoryType::KANJI, CategoryType::KANJINUMERIC];
+        let cats_num_kanji_kana = [
+            CategoryType::NUMERIC,
+            CategoryType::KANJI,
+            CategoryType::KATAKANA,
+        ];
+        let cats_num_alpha_kana = [
+            CategoryType::NUMERIC,
+            CategoryType::ALPHA,
+            CategoryType::KATAKANA,
+        ];
+
+        let path = PathBuf::from(TEST_RESOURCE_DIR).join("read_character_definition.tmp");
+
+        // 1.
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("#\n \n".as_bytes()).unwrap();
+            writer.write("0x0030..0x0039 NUMERIC\n".as_bytes()).unwrap();
+            writer.write("0x0032         KANJI\n".as_bytes()).unwrap();
+        }
+        let cat =
+            CharacterCategory::from_file(path.clone()).expect("Failed to load tmp char def file");
+
+        assert_eq!(
+            cats_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0030).unwrap())
+        );
+        assert_eq!(
+            cats_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0031).unwrap())
+        );
+        assert_eq!(
+            cats_kanji_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0032).unwrap())
+        );
+        assert_eq!(
+            cats_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0033).unwrap())
+        );
+        assert_eq!(
+            cats_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0039).unwrap())
+        );
+
+        // 2.
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("#\n \n".as_bytes()).unwrap();
+            writer.write("0x0030..0x0039 NUMERIC\n".as_bytes()).unwrap();
+            writer.write("0x0070..0x0079 ALPHA\n".as_bytes()).unwrap();
+            writer.write("0x3007         KANJI\n".as_bytes()).unwrap();
+            writer.write("0x0030         KANJI\n".as_bytes()).unwrap();
+        }
+        let cat =
+            CharacterCategory::from_file(path.clone()).expect("Failed to load tmp char def file");
+        assert_eq!(
+            cats_kanji_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0030).unwrap())
+        );
+        assert_eq!(
+            cats_num.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0039).unwrap())
+        );
+        assert_eq!(
+            cats_kanji.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x3007).unwrap())
+        );
+        assert_eq!(
+            cats_default.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0069).unwrap())
+        );
+        assert_eq!(
+            cats_alp.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0070).unwrap())
+        );
+        assert_eq!(
+            cats_default.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0080).unwrap())
+        );
+
+        // 3.
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("#\n \n".as_bytes()).unwrap();
+            writer
+                .write("0x0030..0x0039 KATAKANA\n".as_bytes())
+                .unwrap();
+            writer
+                .write("0x3007         KANJI KANJINUMERIC\n".as_bytes())
+                .unwrap();
+            writer
+                .write("0x3008         KANJI KANJINUMERIC\n".as_bytes())
+                .unwrap();
+            writer
+                .write("0x3009         KANJI KANJINUMERIC\n".as_bytes())
+                .unwrap();
+            writer.write("0x0039..0x0040 ALPHA\n".as_bytes()).unwrap();
+            writer.write("0x0030..0x0039 NUMERIC\n".as_bytes()).unwrap();
+            writer.write("0x0030         KANJI\n".as_bytes()).unwrap();
+        }
+        let cat =
+            CharacterCategory::from_file(path.clone()).expect("Failed to load tmp char def file");
+        assert_eq!(
+            cats_default.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0029).unwrap())
+        );
+        assert_eq!(
+            cats_num_kanji_kana
+                .iter()
+                .map(|v| *v)
+                .collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0030).unwrap())
+        );
+        assert_eq!(
+            cats_num_alpha_kana
+                .iter()
+                .map(|v| *v)
+                .collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0039).unwrap())
+        );
+        assert_eq!(
+            cats_alp.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0040).unwrap())
+        );
+        assert_eq!(
+            cats_default.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x0041).unwrap())
+        );
+        assert_eq!(
+            cats_kanjinumeric
+                .iter()
+                .map(|v| *v)
+                .collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x3007).unwrap())
+        );
+        assert_eq!(
+            cats_default.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x4007).unwrap())
+        );
+
+        // 4.
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("#\n \n".as_bytes()).unwrap();
+            writer.write("0x4E00..0x9FFF KANJI\n".as_bytes()).unwrap();
+            writer
+                .write("0x4E8C         KANJI KANJINUMERIC\n".as_bytes())
+                .unwrap();
+        }
+        let cat =
+            CharacterCategory::from_file(path.clone()).expect("Failed to load tmp char def file");
+
+        assert_eq!(
+            cats_kanji.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types('男')
+        );
+        assert_eq!(
+            cats_kanji.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x4E8B).unwrap())
+        );
+        assert_eq!(
+            cats_kanjinumeric
+                .iter()
+                .map(|v| *v)
+                .collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x4E8C).unwrap())
+        );
+        assert_eq!(
+            cats_kanji.iter().map(|v| *v).collect::<CategoryTypes>(),
+            cat.get_category_types(char::from_u32(0x4E8D).unwrap())
+        );
+
+        // clean up
+        fs::remove_file(path).expect("Failed to remove tmp file");
     }
 
     #[test]
+    #[should_panic]
     fn read_character_definition_with_invalid_format() {
-        // todo: immpl after file read
+        let path = PathBuf::from(TEST_RESOURCE_DIR)
+            .join("read_character_definition_with_invalid_format.tmp");
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("0x0030..0x0039\n".as_bytes()).unwrap();
+        }
+        CharacterCategory::from_file(path.clone()).unwrap();
     }
 
     #[test]
+    #[should_panic]
     fn read_character_definition_with_invalid_range() {
-        // todo: immpl after file read
+        let path = PathBuf::from(TEST_RESOURCE_DIR)
+            .join("read_character_definition_with_invalid_range.tmp");
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("0x0030..0x0029 NUMERIC\n".as_bytes()).unwrap();
+        }
+        CharacterCategory::from_file(path.clone()).unwrap();
     }
 
     #[test]
+    #[should_panic]
     fn read_character_definition_with_invalid_type() {
-        // todo: immpl after file read
+        let path = PathBuf::from(TEST_RESOURCE_DIR)
+            .join("read_character_definition_with_invalid_type.tmp");
+        {
+            let mut writer =
+                BufWriter::new(File::create(&path).expect("Failed to create tmp file"));
+            writer.write("0x0030..0x0039 FOO\n".as_bytes()).unwrap();
+        }
+        CharacterCategory::from_file(path.clone()).unwrap();
     }
 }
