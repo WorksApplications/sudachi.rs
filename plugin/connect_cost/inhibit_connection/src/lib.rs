1aec1575aae24659ce5d53123624aac60b0e5a3b
mh-northlander <mh.northlander@gmail.com>
2021-09-03 14:26:12 +0900

    Add inhibit-connection plugin test

--- plugin/connect_cost/inhibit_connection/src/lib.rs
+++ plugin/connect_cost/inhibit_connection/src/lib.rs
@@ -62,3 +62,38 @@ impl EditConnectionCostPlugin for InhibitConnectionPlugin {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn edit() {
+        let left = 0;
+        let right = 0;
+        let bytes = build_mock_bytes();
+        let mut grammar = build_mock_grammar(&bytes);
+        let mut plugin = InhibitConnectionPlugin::default();
+        plugin.inhibit_pairs = vec![(left, right)];
+
+        plugin.edit(&mut grammar);
+        assert_eq!(
+            Grammar::INHIBITED_CONNECTION,
+            grammar
+                .get_connect_cost(left, right)
+                .expect("Failed to get cost during test")
+        );
+    }
+
+    fn build_mock_bytes() -> Vec<u8> {
+        let mut buf = Vec::new();
+        // set 0 for all of pos size, left and right id size
+        buf.extend(&(0 as i16).to_le_bytes());
+        buf.extend(&(0 as i16).to_le_bytes());
+        buf.extend(&(0 as i16).to_le_bytes());
+        buf
+    }
+    fn build_mock_grammar(bytes: &[u8]) -> Grammar {
+        Grammar::new(bytes, 0).expect("Failed to create grammar")
+    }
+}
