4138cb4da5ca75ef1ef503f752db78be02cfe60d
mh-northlander <mh.northlander@gmail.com>
2021-10-14 14:25:02 +0900

    Load sudachidict dictionary (#93)
    * Load sudachidict_* dictionary
    
    * Load sudachidict_core if no dictionary specified
    
    * wip: Split path resolution as python code
    
    * Fix path join
    
    * Clean up unnecessary code
    
    * Move dict_path function to __init__.py

--- python/py_src/sudachi/__init__.py
+++ python/py_src/sudachi/__init__.py
@@ -6,3 +6,27 @@ from .sudachi import (
     Morpheme,
     WordInfo
 )
+
+from importlib import import_module
+from importlib.util import find_spec
+from pathlib import Path
+
+
+def _get_absolute_dict_path(dict_type: str) -> str:
+    pkg_path = Path(import_module(f'sudachidict_{dict_type}').__file__).parent
+    dic_path = pkg_path / 'resources' / 'system.dic'
+    return str(dic_path.absolute())
+
+
+def _find_dict_path(dict_type='core'):
+    if dict_type not in ['small', 'core', 'full']:
+        raise ValueError('"dict_type" must be "small", "core", or "full".')
+
+    is_installed = find_spec(f'sudachidict_{dict_type}')
+    if is_installed:
+        return _get_absolute_dict_path(dict_type)
+    else:
+        raise ModuleNotFoundError(
+            f'Package `sudachidict_{dict_type}` does not exist. '
+            f'You may install it with a command `$ pip install sudachidict_{dict_type}`'
+        )
--- python/src/dictionary.rs
+++ python/src/dictionary.rs
@@ -19,6 +19,7 @@ use std::sync::Arc;
 
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
+use pyo3::types::PyString;
 
 use sudachi::analysis::stateless_tokenizer::StatelessTokenizer;
 use sudachi::config::Config;
@@ -38,11 +39,26 @@ pub struct PyDictionary {
 impl PyDictionary {
     /// Creates a sudachi dictionary
     #[new]
-    #[args(config_path = "None", resource_dir = "None")]
-    fn new(config_path: Option<PathBuf>, resource_dir: Option<PathBuf>) -> PyResult<Self> {
-        let config = Config::new(config_path, resource_dir, None).map_err(|e| {
+    #[args(config_path = "None", resource_dir = "None", dict_type = "None")]
+    fn new(
+        py: Python,
+        config_path: Option<PathBuf>,
+        resource_dir: Option<PathBuf>,
+        dict_type: Option<&str>,
+    ) -> PyResult<Self> {
+        let dict_path = match dict_type {
+            None => None,
+            Some(dt) => Some(find_dict_path(py, dt)?),
+        };
+
+        let mut config = Config::new(config_path, resource_dir, dict_path).map_err(|e| {
             PyException::new_err(format!("Error loading config: {}", e.to_string()))
         })?;
+
+        // sudachi.json does not have systemDict key or its value is ""
+        if config.system_dict.is_none() || config.system_dict.as_ref().unwrap().is_dir() {
+            config.system_dict = Some(find_dict_path(py, "core")?);
+        }
 
         let dictionary = Arc::new(JapaneseDictionary::from_cfg(&config).map_err(|e| {
             PyException::new_err(format!(
@@ -71,4 +87,13 @@ impl PyDictionary {
     fn close(&mut self) {
         self.dictionary = None;
     }
+}
+
+fn find_dict_path(py: Python, dict_type: &str) -> PyResult<PathBuf> {
+    let pyfunc = PyModule::import(py, "sudachi")?.getattr("_find_dict_path")?;
+    let path = pyfunc
+        .call1((dict_type,))?
+        .cast_as::<PyString>()?
+        .to_str()?;
+    Ok(PathBuf::from(path))
 }
