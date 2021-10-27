733216b4af7cc2d11973d086a479fc87470cf373
mh-northlander <mh.northlander@gmail.com>
2021-10-27 14:48:53 +0900

    Copy comments from rust src to pyi

--- python/py_src/sudachipy/sudachipy.pyi
+++ python/py_src/sudachipy/sudachipy.pyi
@@ -3,46 +3,169 @@ import sudachipy
 
 
 class Dictionary:
-    @classmethod
-    def __init__(self, config_path: str = ...,
-                 resource_dir: str = ...,  dict_type: str = None) -> None: ...
+    """
+    A sudachi dictionary.
+    """
 
-    def close(self) -> None: ...
-    def create(self, mode: SplitMode = SplitMode.C) -> Tokenizer: ...
+    @classmethod
+    def __init__(self, config_path: str = ..., resource_dir: str = ..., dict_type: str = None) -> None:
+        """
+        Creates a sudachi dictionary.
+
+        If both config.systemDict and dict_type are not given, `sudachidict_core` is used.
+        If both config.systemDict and dict_type are given, dict_type is used.
+        """
+        ...
+
+    def close(self) -> None:
+        """
+        Close this dictionary.
+        """
+        ...
+
+    def create(self, mode: SplitMode = SplitMode.C) -> Tokenizer:
+        """
+        Creates a sudachi tokenizer.
+
+        Provide mode to set tokenizer's default split mode (C by default).
+        """
+        ...
 
 
 class Morpheme:
+    """
+    A morpheme (basic semantic unit of language).
+    """
     @classmethod
     def __init__(self) -> None: ...
-    def begin(self) -> int: ...
-    def dictionary_form(self) -> str: ...
-    def dictionary_id(self) -> int: ...
-    def end(self) -> int: ...
-    def get_word_info(self) -> WordInfo: ...
-    def is_oov(self) -> bool: ...
-    def normalized_form(self) -> str: ...
-    def part_of_speech(self) -> List[str]: ...
-    def part_of_speech_id(self) -> int: ...
-    def reading_form(self) -> str: ...
-    def split(self, mode: SplitMode) -> MorphemeList: ...
-    def surface(self) -> str: ...
-    def synonym_group_ids(self) -> List[int]: ...
-    def word_id(self) -> int: ...
+
+    def begin(self) -> int:
+        """
+        Returns the begin index of this in the input text.
+        """
+        ...
+
+    def dictionary_form(self) -> str:
+        """
+        Returns the dictionary form.
+        """
+        ...
+
+    def dictionary_id(self) -> int:
+        """
+        Returns the dictionary id which this word belongs.
+        """
+        ...
+
+    def end(self) -> int:
+        """
+        Returns the end index of this in the input text.
+        """
+        ...
+
+    def get_word_info(self) -> WordInfo:
+        """
+        Returns the word info.
+        """
+        ...
+
+    def is_oov(self) -> bool:
+        """
+        Returns whether if this is out of vocabulary word.
+        """
+        ...
+
+    def normalized_form(self) -> str:
+        """
+        Returns the normalized form.
+        """
+        ...
+
+    def part_of_speech(self) -> List[str]:
+        """
+        Returns the part of speech.
+        """
+        ...
+
+    def part_of_speech_id(self) -> int:
+        """
+        Returns the id of the part of speech in the dictionary.
+        """
+        ...
+
+    def reading_form(self) -> str:
+        """
+        Returns the reading form.
+        """
+        ...
+
+    def split(self, mode: SplitMode) -> MorphemeList:
+        """
+        Returns a list of morphemes splitting itself with given split mode.
+        """
+        ...
+
+    def surface(self) -> str:
+        """
+        Returns the surface.
+        """
+        ...
+
+    def synonym_group_ids(self) -> List[int]:
+        """
+        Returns the list of synonym group ids.
+        """
+        ...
+
+    def word_id(self) -> int:
+        """
+        Returns word id of this word in the dictionary.
+        """
+        ...
 
 
 class MorphemeList:
+    """
+    A list of morphemes.
+    """
     @classmethod
     def __init__(self) -> None: ...
+
     @classmethod
-    def empty(dict) -> MorphemeList: ...
-    def get_internal_cost(self) -> int: ...
-    def size(self) -> int: ...
+    def empty(dict) -> MorphemeList:
+        """
+        Returns an empty morpheme list with dictionary.
+        """
+        ...
+
+    def get_internal_cost(self) -> int:
+        """
+        Returns the total cost of the path.
+        """
+        ...
+
+    def size(self) -> int:
+        """
+        Returns the number of morpheme in this list.
+        """
+        ...
+
     def __getitem__(self, index) -> Morpheme: ...
     def __iter__(self) -> Iterator[Morpheme]: ...
     def __len__(self) -> int: ...
 
 
 class SplitMode:
+    """
+    Unit to split text.
+
+    A == short mode
+
+    B == middle mode
+
+    C == long mode
+    """
+
     A: ClassVar[SplitMode] = ...
     B: ClassVar[SplitMode] = ...
     C: ClassVar[SplitMode] = ...
@@ -54,8 +177,15 @@ class Tokenizer:
     SplitMode: ClassVar[sudachipy.SplitMode] = ...
     @classmethod
     def __init__(self) -> None: ...
-    def tokenize(self, text: str,
-                 mode: sudachipy.SplitMode = ...) -> MorphemeList: ...
+
+    def tokenize(self, text: str, mode: sudachipy.SplitMode = ...) -> MorphemeList:
+        """
+        Break text into morphemes.
+
+        By default tokenizer's split mode is used.
+        The logger provided is ignored.
+        """
+        ...
 
 
 class WordInfo:
