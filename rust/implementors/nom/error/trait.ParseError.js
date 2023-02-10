3ddb45aaf694554a3682b08e61684e0e4da8f3e6
eiennohito <eiennohito@users.noreply.github.com>
2023-02-10 00:30:37 +0000

    deploy: 7f43a12a19ee06c3e75079a52e60a4007b5d6596

--- rust/implementors/nom/error/trait.ParseError.js
+++ rust/implementors/nom/error/trait.ParseError.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["sudachi"] = [{"text":"impl&lt;I&gt; ParseError&lt;I&gt; for <a class=\"enum\" href=\"sudachi/error/enum.SudachiNomError.html\" title=\"enum sudachi::error::SudachiNomError\">SudachiNomError</a>&lt;I&gt;","synthetic":false,"types":["sudachi::error::SudachiNomError"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"sudachi":[["impl&lt;I&gt; ParseError&lt;I&gt; for <a class=\"enum\" href=\"sudachi/error/enum.SudachiNomError.html\" title=\"enum sudachi::error::SudachiNomError\">SudachiNomError</a>&lt;I&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
