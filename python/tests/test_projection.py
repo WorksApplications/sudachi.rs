da4888c847b9fcb3506b3be46eaf1a439df1b231
Arseny Tolmachev <arseny@kotonoha.ws>
2023-08-14 14:36:10 +0900

    fix copyright year

--- python/tests/test_projection.py
+++ python/tests/test_projection.py
@@ -1,4 +1,4 @@
-#   Copyright (c) $YEAR Works Applications Co., Ltd.
+#   Copyright (c) 2023 Works Applications Co., Ltd.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
