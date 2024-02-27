a0b515bdfe2aa8f11b6e86c304a035ba7a6a8a94
mh-northlander <mh-northlander@users.noreply.github.com>
2024-02-27 02:35:53 +0000

    deploy: 4be2781140fc4d2a98884a98e86327b87f527038

--- rust/trait.impl/core/cmp/trait.PartialOrd.js
+++ rust/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"sudachi":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"sudachi/dic/word_id/struct.WordId.html\" title=\"struct sudachi::dic::word_id::WordId\">WordId</a>"]]
+"sudachi":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"sudachi/dic/word_id/struct.WordId.html\" title=\"struct sudachi::dic::word_id::WordId\">WordId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
