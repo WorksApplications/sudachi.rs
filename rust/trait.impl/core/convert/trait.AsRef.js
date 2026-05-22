edb95f576b6938c44cc36eea8d69f1d0833250ab
hayashi-mas-wap <hayashi-mas-wap@users.noreply.github.com>
2026-05-22 08:23:32 +0000

    deploy: 750192fc736443a2c6661a27e885e0ac5c1c9fc9

--- rust/trait.impl/core/convert/trait.AsRef.js
+++ rust/trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"enum\" href=\"sudachi/dic/storage/enum.Storage.html\" title=\"enum sudachi::dic::storage::Storage\">Storage</a>",0]]]]);
+    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.95.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"enum\" href=\"sudachi/dic/storage/enum.Storage.html\" title=\"enum sudachi::dic::storage::Storage\">Storage</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- rust/trait.impl/core/convert/trait.TryFrom.js
+++ rust/trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"sudachi/config/enum.SurfaceProjection.html\" title=\"enum sudachi::config::SurfaceProjection\">SurfaceProjection</a>",0]]]]);
+    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.95.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"sudachi/config/enum.SurfaceProjection.html\" title=\"enum sudachi::config::SurfaceProjection\">SurfaceProjection</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
