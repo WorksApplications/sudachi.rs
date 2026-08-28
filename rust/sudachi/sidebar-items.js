6d2c3ffe45390c421ac8a1f2287ffc5b4827d51b
hayashi-mas-wap <hayashi-mas-wap@users.noreply.github.com>
2026-08-28 08:44:17 +0000

    deploy: 2f5653ebffb5da20207c69522910126f5103d80d

--- rust/sudachi/sidebar-items.js
+++ rust/sudachi/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["sudachi_dso_plugin"],"mod":["analysis","config","dic","error","input_text","plugin","pos","prelude","sentence_detector","sentence_splitter"]};
+window.SIDEBAR_ITEMS = {"macro":[["sudachi_dso_plugin",1]],"mod":["analysis","config","dic","error","input_text","plugin","pos","prelude","sentence_detector","sentence_splitter"]};
--- rust/trait.impl/core/cmp/trait.PartialOrd.js
+++ rust/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"sudachi/dic/word_id/struct.WordId.html\" title=\"struct sudachi::dic::word_id::WordId\">WordId</a>",0]]]]);
+    const implementors = Object.fromEntries([["sudachi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"sudachi/dic/word_id/struct.WordId.html\" title=\"struct sudachi::dic::word_id::WordId\">WordId</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
