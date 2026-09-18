4f5f62f7a460923cdf8ca48e397d94b321cdd7dc
hayashi-mas-wap <hayashi-mas-wap@users.noreply.github.com>
2026-09-18 12:47:10 +0000

    deploy: 1d86a05bccfaea247dab27cf20ce98dde0814d46

--- python/_static/documentation_options.js
+++ python/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.6.12-a1',
+    VERSION: '0.7.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ rust/search.index/00a77991fb.js
@@ -0,0 +1,1 @@
+rn_("CQAbA8MAAAguNTQ7wVoAdXY=")
